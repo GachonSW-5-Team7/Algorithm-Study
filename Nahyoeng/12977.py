6a9e4803244de5a27243fa44d9d3960fe407e3d0
Minjae Seon <ddol0225@gmail.com>
2021-08-26 13:59:10 +0900

    Fixed Conflict File

--- Nahyoeng/12977.py
+++ Nahyoeng/12977.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def solution(nums):
     
     import numpy as np
@@ -18,25 +17,4 @@ def solution(nums):
         if is_prime(num):
             answer+=1
                 
-=======
-def solution(nums):
-    
-    import numpy as np
-    from itertools import combinations
-    
-    def is_prime(x):
-        for i in range(2,x):
-            if x%i==0:
-                return False
-        return True
-    
-    combi=list(combinations(nums,3))
-    answer=0
-    
-    for i in range (len(combi)):
-        num=np.sum(combi[i])
-        if is_prime(num):
-            answer+=1
-                
->>>>>>> 43b43477f93417a27790e62dc22313fdb7dd94ca
     return answer
--- Nahyoeng/77480.py
+++ Nahyoeng/77480.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def solution(lottos, win_nums):
     
     import numpy as np
@@ -12,19 +11,4 @@ def solution(lottos, win_nums):
         max+=1
     
     answer = [7-max,7-min]
-=======
-def solution(lottos, win_nums):
-    
-    import numpy as np
-    
-    min = len(set(lottos)&set(win_nums))
-    max = min+lottos.count(0)
-    
-    if min==0:
-        min+=1
-    if max==0:
-        max+=1
-    
-    answer = [7-max,7-min]
->>>>>>> 43b43477f93417a27790e62dc22313fdb7dd94ca
     return answer
